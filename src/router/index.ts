1b205df19f849666a33b7cfd1d349cd9b69a107a
emmanueluwa <emmanuel@fulodev.com>
2024-04-06 18:03:02 +0100

    initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,50 @@
+// Composables
+import { createRouter, createWebHistory } from "vue-router";
+import { useSupabaseClient } from "@/composables/supabase";
+
+const loginRoute = { name: "Login" };
+const loggedInRoute = { name: "Home" };
+
+const loginGuard = async (to: any, from: any, next: Function) => {
+  const { data } = await useSupabaseClient.auth.getSession();
+  if (data.session) {
+    next();
+  } else {
+    next(loginRoute);
+  }
+};
+
+const loggedInGuard = async (to: any, from: any, next: Function) => {
+  const { data } = await useSupabaseClient.auth.getSession();
+  if (data.session) {
+    next(loggedInRoute);
+  } else {
+    next();
+  }
+};
+
+const routes = [
+  {
+    path: "/",
+    component: () => import("@/layouts/default/Default.vue"),
+    children: [
+      {
+        path: "",
+        name: "Home",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
+        beforeEnter: loginGuard,
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
