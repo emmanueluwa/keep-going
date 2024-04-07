e596a4439c70f66ee79ccda7fde3d32fc4b73163
emmanueluwa <emmanuel@fulodev.com>
2024-04-07 11:46:43 +0100

    adding auth middleware

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,29 @@
 // Composables
 import { createRouter, createWebHistory } from "vue-router";
+import { useSupabaseClient } from "../composables/supabase";
+
+const loginRoute = { name: "Login" };
+const loggedInRoute = { name: "Home" };
+
+const loginGuard = async (to: any, from: any, next: Function) => {
+  const { data } = await useSupabaseClient.auth.getSession();
+
+  if (data.session) {
+    next();
+  } else {
+    next(loginRoute);
+  }
+};
+
+const loggedInGuard = async (to: any, from: any, next: Function) => {
+  const { data } = await useSupabaseClient.auth.getSession();
+
+  if (data.session) {
+    next(loggedInRoute);
+  } else {
+    next();
+  }
+};
 
 const routes = [
   {
@@ -14,6 +38,13 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () =>
           import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
+        beforeEnter: loginGuard,
+      },
+      {
+        path: "login",
+        name: "Login",
+        component: () => import("../views/Home.vue"),
+        beforeEnter: loggedInGuard,
       },
     ],
   },
