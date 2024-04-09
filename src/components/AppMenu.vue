f655effed02995dfa2f1a81ddbdb792579dde260
emmanueluwa <emmanuel@fulodev.com>
2024-04-09 16:41:47 +0100

    updating appmenu component

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -11,16 +11,34 @@ const { userIsLoggedIn } = storeToRefs(userStore);
 const goToPage = (page: string): void => {
   appStore.navigateToPage(page);
 };
+
+const menuItems = [
+  {
+    icon: "mdi-dumbell",
+    title: "Track",
+    page: "/track",
+  },
+];
 </script>
 
 <template>
   <v-navigation-drawer v-model="drawer" app>
     <v-list dense v-if="userIsLoggedIn">
+      <v-list-item
+        v-for="item in menuItems"
+        :key="item.title"
+        @click="goToPage(item.page)"
+      >
+        <template v-slot:prepend>
+          <v-icon :icon="item.icon"></v-icon>
+        </template>
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
       <v-list-item @click="userStore.logout()">
         <template v-slot:prepend>
           <v-icon icon="mdi-account-arrow-right"></v-icon>
         </template>
-        <v-list-item-title>Log out</v-list-item-title>
+        <v-list-item-title>Logout</v-list-item-title>
       </v-list-item>
     </v-list>
     <v-list dense v-else>
---
+++ src/views/Track.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup>
+import TrackExercise from "../components/TrackExercise.vue";
+</script>
+
+<template><track-exercise /></template>
